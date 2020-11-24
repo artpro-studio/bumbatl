600a54b26b461197056b0f5e8348b8eb7eaa5cdb
andreyi96 <ya.andreyi96@yandex.ru>
2020-11-25 01:34:40 +0300

    новости

--- app/js/common.js
+++ app/js/common.js
@@ -67,5 +67,10 @@ $(function() {
             }
         }
     });
-
+    $('.js--news-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: $('.news .prev'),
+        nextArrow: $('.news .next'),
+    });
 });
